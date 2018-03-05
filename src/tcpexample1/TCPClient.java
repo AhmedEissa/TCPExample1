5da2de50a36e075675b8dcb4db0967732f23bd5c
Matthew <matbulat@gmail.com>
2018-03-05 11:08:09 +0000

    Simplified the example

--- src/tcpexample1/TCPClient.java
+++ src/tcpexample1/TCPClient.java
@@ -9,22 +9,18 @@ class TCPClient {
         String sentence;
         String modifiedSentence;
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        
+
         Socket clientSocket = new Socket("localhost", 6789);
-        
-        while(true){        
+
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        
+
         System.out.println("Type your text: \n or type end to terminate connection.");
         sentence = inFromUser.readLine();
-        if(sentence.equals("end")){
-            break;
-        }        
+
         outToServer.writeBytes(sentence + '\n');
         modifiedSentence = inFromServer.readLine();
         System.out.println("FROM SERVER: " + modifiedSentence);
-        }
         clientSocket.close();
     }
 }
--- src/tcpexample1/TCPServer.java
+++ src/tcpexample1/TCPServer.java
@@ -10,14 +10,13 @@ class TCPServer {
         String capitalizedSentence;
         ServerSocket welcomeSocket = new ServerSocket(6789);
         Socket connectionSocket = welcomeSocket.accept();
-        while (true) {            
-            BufferedReader inFromClient
-                    = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
-        }
+        BufferedReader inFromClient
+                = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+        clientSentence = inFromClient.readLine();
+        System.out.println("Received: " + clientSentence);
+        capitalizedSentence = clientSentence.toUpperCase() + '\n';
+        outToClient.writeBytes(capitalizedSentence);
+
     }
 }
